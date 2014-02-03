54081d00c96067ba4feb19bbfbe6101c9e8b80d4
jdiez17 <jose.manuel.diez@gmail.com>
2014-02-03 15:43:33 +0000

    Unoptimised, but fully functional

--- src/day-progress.c
+++ src/day-progress.c
@@ -1,6 +1,7 @@
 #include <pebble.h>
 
-// 3.57 pixels per second
+// 86400 / (144 * 168)
+#define SECONDS_PER_PIXEL 3.57 
 
 Window* window;
 Layer* root_layer;
@@ -9,51 +10,61 @@ struct tm last_time;
 void clear_screen(GContext* ctx) {
     graphics_context_set_fill_color(ctx, GColorWhite);
     graphics_fill_rect(ctx, (GRect) {
-            .origin = { .x = 0, .y = 0 },
-            .size = { .h = 144, .w = 168}
+        .origin = GPointZero,
+        .size = GSize(144, 168)
     }, 0, GCornerNone);
     graphics_context_set_fill_color(ctx, GColorBlack);
 }
 
 void layer_update(Layer* l, GContext* ctx) {
     int seconds_so_far = last_time.tm_hour * 3600 + last_time.tm_min * 60 + last_time.tm_sec;
+    int pixels_so_far = seconds_so_far / SECONDS_PER_PIXEL;
+
     clear_screen(ctx); // TODO: Optimize this
 
     // First: draw a rectangle that represents full lines so far
-    int lines = seconds_so_far / 168;
+    int lines = (pixels_so_far / 144);
 
-    APP_LOG(APP_LOG_LEVEL_DEBUG, "Seconds so far: %d, lines: %d", seconds_so_far, lines);
     graphics_fill_rect(ctx, (GRect) {
-        .origin = { .x = 0, .y = 0 },
-        .size = { .h = lines, .w = 168 }
+        .origin = GPointZero,
+        .size = GSize(144, lines)
     }, 0, GCornerNone);
+
+    // Second: draw a line from the beginning of the next line that represents seconds
+    int stray_pixels = pixels_so_far % 144;
+    
+    graphics_draw_line(ctx, GPoint(0, lines), GPoint(stray_pixels, lines));
+
+    // Last: log info
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "Seconds so far: %d (%d pixels), lines: %d, stray pixels: %d", seconds_so_far, pixels_so_far, lines, stray_pixels);
 }
 
 void handle_tick(struct tm* t, TimeUnits delta_t) {
     last_time = *t; // copy to static variable
-
-    APP_LOG(APP_LOG_LEVEL_DEBUG, "Tick!");
     layer_mark_dirty(root_layer);
 }
 
 void init(void) {
-  window = window_create();
-  window_stack_push(window, true);
+    window = window_create();
+    window_stack_push(window, true);
 
-  root_layer = window_get_root_layer(window);
-  layer_set_update_proc(root_layer, layer_update);
+    root_layer = window_get_root_layer(window);
+    layer_set_update_proc(root_layer, layer_update);
 
-  tick_timer_service_subscribe(SECOND_UNIT, handle_tick);
+    tick_timer_service_subscribe(SECOND_UNIT, handle_tick);
+
+    time_t now = time(NULL);
+    last_time = *localtime(&now); 
 }
 
 void deinit(void) {
-  window_destroy(window);
+    window_destroy(window);
 
-  tick_timer_service_unsubscribe();
+    tick_timer_service_unsubscribe();
 }
 
 int main(void) {
-  init();
-  app_event_loop();
-  deinit();
+    init();
+    app_event_loop();
+    deinit();
 }
